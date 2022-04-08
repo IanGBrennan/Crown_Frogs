7feff7e1db56fbedf1acd037d332b113914e27d6
IanGBrennan <Ian.Brennan@anu.edu.au>
2022-04-08 10:26:36 +1000

    MMHH Alignments

--- Myobatrachidae/Alignments/MACSE_Shells/MACSE_Shell16.sh
+++
@@ -1,6 +0,0 @@
-cd /Users/ianbrennan/Documents/GitHub/Crown_Frogs/Myobatrachidae/Original_Alignments
-java -jar /Applications/macse_v2.03.jar -prog refineAlignment -align /Users/ianbrennan/Documents/GitHub/Crown_Frogs/Myobatrachidae/Original_Alignments/Myobatrachidae_L98.fasta -optim 1 -local_realign_init 0.1 -local_realign_dec 0.1 -fs 10
-java -jar /Applications/macse_v2.03.jar -prog refineAlignment -align /Users/ianbrennan/Documents/GitHub/Crown_Frogs/Myobatrachidae/Original_Alignments/Myobatrachidae_L99.fasta -optim 1 -local_realign_init 0.1 -local_realign_dec 0.1 -fs 10
-echo finished batch 16 of 16
-perl -pi -w -e 's/!/N/g;' *.fasta
-exit 0
